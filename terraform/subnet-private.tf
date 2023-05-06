09dbe7a60431393e222566ee1c088180cfe02809
Davinderpro01 <d-alex01@hotmail.com>
2023-05-06 15:59:32 -0600

    COMMIT FINAL

---
+++ terraform/subnet-private.tf
@@ -0,0 +1,16 @@
+resource "aws_subnet" "private-subnet" {
+  vpc_id = aws_vpc.this.id
+  cidr_block = local.vpc.cidr_subnet_private
+  map_public_ip_on_launch = false
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "${local.prefix}-private-subnet"
+    Environment = local.env
+    Path = "${basename(abspath(path.module))}/subnet-private.tf"
+}
+
+depends_on = [
+    aws_vpc.this
+    ]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+locals {
+  prefix = "parcial"
+  env = "dev"
+  aws = {
+    region = "us-east-1"
+  }
+  vpc = {
+    cidr = "192.168.0.0/16"
+    cidr_subnet_public = "192.168.0.0/24"
+    cidr_subnet_private = "192.168.1.0/24"
+  }
+}
